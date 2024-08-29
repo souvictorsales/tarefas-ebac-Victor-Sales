a178aef56362a117c13c78fe1ef2dabdd72ea7c6
Victor Sales <victorsales.s@gmail.com>
2024-08-29 19:29:40 -0300

    Calculo

---
+++ CalculoMedia.java
@@ -0,0 +1,16 @@
+public class CalculoMedia {
+    public static void main(String args[]) {
+        calculoMedia();
+    }
+
+    public static void calculoMedia() {
+        int num1 = 10;
+        int num2 = 7;
+        int num3 = 5;
+        int num4 = 8;
+
+        int num5 = (num1 + num2 + num3 + num4)/4;
+
+        System.out.println(num5);
+    }
+}
