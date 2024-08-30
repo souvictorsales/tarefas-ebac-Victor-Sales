e2ac8e454266a817c6c071be2283c15b8fe9c144
Victor Sales <victorsales.s@gmail.com>
2024-08-30 19:36:44 -0300

    Exercicio Módulo 10

---
+++ Modulo10/Boletim.java
@@ -0,0 +1,24 @@
+package Modulo10;
+import java.util.Scanner;
+
+public class Boletim {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    final int NUM_NOTAS = 4;
+    double somaNotas = 0.0;
+
+    
+    for (double i = 1; i <= NUM_NOTAS; i++) {
+        System.out.println("Informe a nota " + i + ":");
+        double nota = scanner.nextDouble();
+        
+        somaNotas += nota;
+    }
+
+    double media = somaNotas / NUM_NOTAS;
+
+    System.out.printf("A média das quatro notas do aluno(a) é: %.2f%n", media);
+
+    scanner.close();
+}
+}
